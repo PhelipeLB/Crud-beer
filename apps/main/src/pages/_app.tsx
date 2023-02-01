77772c67e1b52b9cb96ea07284b9f0b25ce8b516
PhelipeLB <phelipeborges92@gmail.com>
2023-02-01 19:34:57 -0300

    feat: added color to theme

--- apps/main/src/pages/_app.tsx
+++ apps/main/src/pages/_app.tsx
@@ -1,5 +1,6 @@
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "@crud-beer/ui";
+import { theme } from '../chakra/theme'
 
 export default function MyApp({ Component, pageProps }) {
   return (
